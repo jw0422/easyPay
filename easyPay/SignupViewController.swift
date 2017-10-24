c1699e75ee791ab6e3b33d3aa0dd44466b3545f5
wuyingyu <yingyuwu@usc.edu>
2017-10-24 13:35:44 -0700

    add iboutlet and ibaction

---
+++ easyPay/SignupViewController.swift
@@ -0,0 +1,50 @@
+//
+//  SignupViewController.swift
+//  easyPay
+//
+//  Created by 吴莹羽 on 2017/10/24.
+//  Copyright © 2017年 Jingyu Wang. All rights reserved.
+//
+
+import UIKit
+import Firebase
+
+class SignupViewController: UIViewController {
+    
+    @IBOutlet weak var emailTF: UITextField!
+    @IBOutlet weak var passwordTF: UITextField!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    @IBAction func createAccountAction(_ sender: Any) {
+        if emailTF.text == "" {
+            let alertController = UIAlertController(title: "Error", message: "Please enter your email and password", preferredStyle: .alert)
+            
+            let defaultAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+            alertController.addAction(defaultAction)
+            
+            present(alertController, animated: true, completion: nil)
+            
+        } else {
+            Auth.auth().createUser(withEmail: emailTF.text!, password: passwordTF.text!) { (user, error) in
+                
+                if error == nil {
+                    print("You have successfully signed up")
+                    //Goes to the Setup page which lets the user take a photo for their profile picture and also chose a username
+                    
+                    let vc = self.storyboard?.instantiateViewController(withIdentifier: "Home")
+                    self.present(vc!, animated: true, completion: nil)
+                    
+                } else {
+                    let alertController = UIAlertController(title: "Error", message: error?.localizedDescription, preferredStyle: .alert)
+                    
+                    let defaultAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+                    alertController.addAction(defaultAction)
+                    
+                    self.present(alertController, animated: true, completion: nil)
+                }
+            }
+        }
+    }
+}
